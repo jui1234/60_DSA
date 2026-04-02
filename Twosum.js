1394d58d9c7b3f2a74ca712474faa79cbb7232d3
jui <juiyezarkar1999@gmail.com>
2026-04-02 20:28:02 +0530

    twosum

--- Twosum.js
+++ Twosum.js
@@ -1,5 +1,13 @@
-export function TwoSum(num1,num2){
-return num1 +num2;
+var twoSum = function(nums, target) {
+   for(let i=0;i<nums.length;i++){
+    for(let j=i+1;j<nums.length;j++){
+        if(nums[i]+nums[j]===target){
+            return [i,j];
+        }
+    }
+   }
+   return [];
 }
-
-console.log(TwoSum(2,3));
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3,2,4], 6));
+console.log(twoSum([3,3], 6));
