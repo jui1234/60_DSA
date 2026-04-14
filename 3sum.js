e35de044b7cee949c162c088bac58fd42282a819
jui <juiyezarkar1999@gmail.com>
2026-04-14 12:34:22 +0530

    3sum dsa

---
+++ 3sum.js
@@ -0,0 +1,36 @@
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    const result = [];
+
+    for(let i=0;i<nums.length-2;i++){
+         if(i > 0 && nums[i] === nums[i-1]) continue;
+       let current = nums[i];
+       let left = i+1;
+       let right = nums.length-1;
+
+       while(left<right){
+            const sum = current + nums[left] + nums[right];
+            if(sum ===0){
+                result.push([current, nums[left], nums[right]].join(','));
+                 // 🔥 skip duplicates
+                while(left < right && nums[left] === nums[left+1]) left++;
+                while(left < right && nums[right] === nums[right-1]) right--;
+
+                left++;
+                right--;
+            }
+            else if(sum<0){
+                left++;
+            }
+            else{
+                right--;
+             }
+       }
+    }
+    return Array.from(result).map(item => item.split(',').map((item)=>+item));
+};
+
+console.log(threeSum([-1,0,1,2,-1,-4]));
+console.log(threeSum([0,1,1]));
+console.log(threeSum([0,0,0]));
+console.log(threeSum([1,2,0,1,0,0,0,0]));
