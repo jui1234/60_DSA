1b4d4ef0269946e91b5e8de018d00e0fa86d3462
jui <juiyezarkar1999@gmail.com>
2026-04-22 21:55:22 +0530

    palindrom

---
+++ palindrom.js
@@ -0,0 +1,20 @@
+const palindrom = function(s){
+let left=0;
+let right=s.length-1;
+
+while(left<right){
+if(s[left]===s[right]){
+    left++;
+    right--;
+}   
+else{
+    return false;       
+}
+
+
+}
+return true;
+}
+
+console.log(palindrom("madam"));
+console.log(palindrom("rcar"));
