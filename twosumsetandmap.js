def642d6bffa42cecfcb4bacbbc365d5414f63dc
jui <juiyezarkar1999@gmail.com>
2026-05-01 22:08:46 +0530

    twosumsetand hashmap

---
+++ twosumsetandmap.js
@@ -0,0 +1,17 @@
+var twoSum = function(nums, target) {
+    let map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+
+        map.set(nums[i], i);
+    }
+};
+
+console.log("result",twoSum([2,7,11,15], 9))
+console.log("result",twoSum([3,2,4], 6))
+console.log("result",twoSum([3,3], 6))
