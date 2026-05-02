d6579850e33ebe83ba96faf22021fd49a29853e2
jui <juiyezarkar1999@gmail.com>
2026-05-02 12:41:18 +0530

    missing number using set

---
+++ missing_number_using_set.js
@@ -0,0 +1,14 @@
+var missingNumber = function(nums) {
+    let seen =new Set(nums);
+
+    for(let i=0;i<nums.length;i++) {   
+        if(!seen.has(i)){
+            return i;
+        } 
+
+    }
+};
+
+console.log("result",missingNumber([3,0,1]))    
+console.log("result",missingNumber([0,1]))
+console.log("result",missingNumber([9,6,4,2,3,5,7,0,1]))
