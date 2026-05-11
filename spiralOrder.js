701f5aab87577471cfb3f5840d0a014bd2f2ffb4
jui <juiyezarkar1999@gmail.com>
2026-05-11 20:30:46 +0530

    done dry run

--- spiralOrder.js
+++ spiralOrder.js
@@ -42,9 +42,13 @@ var spiralOrder = function(matrix) {
     }
 
     return result;
+
+
 };
+
 
 
 
 console.log("result",spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
 console.log("result",spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
+console.log("result",spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]))
